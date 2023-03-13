19baea7b4ea00a052fb6bc3ad14c67e017a9fff4
email <thariq.fswd@gmail.com>
2023-03-13 18:36:26 +0530

    scroll implement

--- src/components/PageComponents/Home/index.js
+++ src/components/PageComponents/Home/index.js
@@ -2,14 +2,27 @@ import React from "react";
 import { Card } from "../../common/UI/Card";
 import { Title } from "../../common/UI/Title";
 import { homeData } from "./constatnt";
+import styles from "./styles.module.css";
 
 export const Home = () => {
   return (
     <div>
+    <div className={styles.upcomingCard}>
       <Title title="Banners" color="#808080" goto="All Banners" />
-      {homeData.map((i, index) => (
+   <div className={styles.listscroll}>
+   {homeData.map((i, index) => (
         <Card key={i.id} {...i} />
       ))}
+      </div> 
+    </div>
+      <div  className={styles.upcomingCard}>
+      <Title title="List" color="#808080" goto="All List" />
+   <div className={styles.listscroll}>
+   {homeData.map((i, index) => (
+        <Card key={i.id} {...i} />
+      ))}
+      </div> 
+    </div>
     </div>
   );
 };
